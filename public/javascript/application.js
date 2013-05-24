a278497f843915e914cd7a106862da952fd64f7a
Dan Mayer <dan@mayerdan.com>
2013-05-24 14:55:36 -0400

    added email storage and show / hide setup

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,44 @@
 (function ($) {
-  console.log('ready')
+
+  if(document.cookie.match(/kindle_mail/)==null) {
+    $('#setupNotice').slideDown('slow');
+  } else {
+    var email = readCookie('kindle_mail');
+    $('input[name="email"]').val(email);
+  }
+
+  $('#setup').click(function() {
+    $('#setupNotice').slideDown('slow');
+    return false;
+  });
+
+  $('#notice-form').submit(function() {
+    var email = $('#kindle-mail').val();
+    if (email.match(/kindle\.com/)!=null) {
+      setCookie('kindle_mail',email,300);
+      $('input[name="email"]').val(email);
+      $('#setupNotice').slideUp();
+    } else {
+      alert('must use a kindle.com or free.kindle.com email address for this to work.')
+    }
+    return false;
+  });
+  
+  function setCookie(c_name,value,exdays) {
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie=c_name + "=" + c_value;
+  }
+
+  function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
 }(jQuery));
