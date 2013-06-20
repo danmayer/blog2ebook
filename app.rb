61fdf50292ac2b721058b6d887dd58aedb3424bd
Dan Mayer <dan@mayerdan.com>
2013-06-19 20:35:46 -0400

    refactoring to clean up the document fetching

--- app.rb
+++ app.rb
@@ -13,7 +13,6 @@ require './lib/redis_initializer'
 
 MAIL_API_KEY = ENV['MAILGUN_API_KEY']
 MAIL_API_URL = "https://api:#{MAIL_API_KEY}@api.mailgun.net/v2/app7941314.mailgun.org"
-READ_API_TOKEN = ENV['READ_API_TOKEN']
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 set :root, File.dirname(__FILE__)
@@ -27,7 +26,6 @@ use Rack::Session::Cookie, :key => 'kindleizer.rack.session',
 use Rack::Flash, :sweep => true
 
 helpers do
-
 end
 
 def self.get_or_post(url,&block)
@@ -54,7 +52,7 @@ end
 get_or_post '/kindleize' do
   verify_url_and_email
   verify_usage
-  doc      = document_from_url(params['url'])
+  doc      = DocumentFetching.new(params['url']).document_from_url
   content  = doc['content']
   title    = doc['title'] 
   to_email = params['email']
@@ -67,7 +65,7 @@ get_or_post '/kindleizeblog' do
   begin
     verify_url_and_email
     verify_usage
-    doc      = document_from_feed(params['url'])
+    doc      = DocumentFetching.new(params['url']).document_from_feed
     content  = doc[:content]
     title    = doc[:title] 
     to_email = params['email']
@@ -192,102 +190,4 @@ def email_to_kindle(title, content, to_email)
   :subject => "kindle book",
   :text => 'kindle book attached',
   :attachment => File.new(book_file)
-end
-
-def document_from_url(url)
-  begin
-    results = RestClient::Request.execute(:method => :get, :url => "http://www.readability.com/api/content/v1/parser?url=#{url}&token=#{READ_API_TOKEN}", :timeout => 10, :open_timeout => 10)
-    json_results = JSON.parse(results)
-    json_results
-  rescue RestClient::GatewayTimeout
-    error_response("Hmmm looks like I can't reach that article.")
-  end
-end
-
-def title_from_feed(xml_doc)
-  title = xml_doc.search('title').first.content
-end
-
-def link_from_entry(entry)
-  entry.search('link').first['href'] || entry.search('link').first.content
-end
-
-def date_from_entry(entry)
-  if entry.search('updated').length > 0
-    post_date = entry.search('updated').first.content
-    post_date = Date.parse(post_date).strftime("%m/%d/%Y at %I:%M%p")
-  else
-    post_date = entry.search('pubDate').first.content
-    post_date = Date.parse(post_date).strftime("%m/%d/%Y at %I:%M%p")
-  end
-end
-
-def content_from_entry(entry)
-  if entry.search('content').length > 0
-    entry.search('content').first.content
-  elsif entry.search('description').length > 0
-    entry.search('description').map(&:content).join("\n")
-  else
-    entry.at_xpath("content:encoded").text
-  end
-end
-
-def contents_from_entry(entry)
-  post_title   = entry.search('title').first.content
-  post_content = content_from_entry(entry)
-  post_date    = date_from_entry(entry)
-  post_link    = link_from_entry(entry)
-  {
-    :title => post_title,
-    :content => post_content,
-    :date => post_date,
-    :link => post_link
-  }
-end
-
-####
-# Method converts data from a feed format to a html kindle book format
-# 
-# Kindle formatting notes:
-# <a name="start" /> — This is the start of the book.
-# It’s where the book will open the first time someone reads it.
-# You can use it to skip over title and dedication pages and get your readers right in the meat of the book.
-# <a name="TOC" /> — Place this at the top of your table of contents.
-# <a name="chap1" /> 
-###
-def document_from_feed(url)
-puts 'doc'
-  results = RestClient.get(url)
-  xml_doc  = Nokogiri::XML::Document.parse(results)
-  
-  title = title_from_feed(xml_doc)
-  content = ''
-  chapters = {}
-
-  xml_doc.search(feed_item_element(xml_doc)).each do |entry|
-    post = contents_from_entry(entry)
-    post_chapter = "chap#{(chapters.length + 1)}"
-    chapters[post[:title]] = post_chapter
-    seperator = "<br/><hr/><a name='#{post_chapter}' /><h3><a href='#{post[:link]}'>#{post[:title]}</a></h3>"
-    seperator += "<div>updated: #{post[:date]}</div><br/>"
-    content   += "#{seperator}#{post[:content]}<mbp:pagebreak />"
-  end
-
-  book_start       = "<a name='start' /><h3>#{title}</h3>"
-  table_of_content = "<a name='TOC' /><h3>Table of Contents</h3><ul>"
-  chapters.each_pair do |post_title, chapter|
-    table_of_content += "<li><a href='##{chapter}'>#{post_title}</a></li>"
-  end
-  table_of_content += "</ul><hr/><mbp:pagebreak />"
-  content = "#{book_start}#{table_of_content}#{content}"
-
-  {:title => title, :content => content}
-end
-
-def feed_item_element(xml_doc)
-  if xml_doc.search("entry").length > 1
-    'entry'
-  else
-    'item'
-  end
 end
--- lib/document_fetching.rb
+++ lib/document_fetching.rb
@@ -2,12 +2,20 @@ require 'pismo'
 require 'readability'
 
 class DocumentFetching
+  READ_API_TOKEN = ENV['READ_API_TOKEN']
 
   def initialize(url)
     @url = url
   end
 
   def document_from_url
+    begin
+      results = RestClient::Request.execute(:method => :get, :url => "http://www.readability.com/api/content/v1/parser?url=#{@url}&token=#{READ_API_TOKEN}", :timeout => 10, :open_timeout => 10)
+      json_results = JSON.parse(results)
+      json_results
+    rescue RestClient::GatewayTimeout
+      error_response("Hmmm looks like I can't reach that article.")
+    end
   end
 
   # Pismo
@@ -17,5 +25,96 @@ class DocumentFetching
   #ruby-readability
   #source = open('http://mayerdan.com/2013/05/08/performance_bugs_cluster/')
   # {:content => Readability::Document.new(source).content}
+
+  ####
+  # Method converts data from a feed format to a html kindle book format
+  # 
+  # Kindle formatting notes:
+  # <a name="start" /> — This is the start of the book.
+  # It’s where the book will open the first time someone reads it.
+  # You can use it to skip over title and dedication pages and get your readers right in the meat of the book.
+  # <a name="TOC" /> — Place this at the top of your table of contents.
+  # <a name="chap1" /> 
+  #
+  # TODO move more to book_formatter
+  ###
+  def document_from_feed
+    results = RestClient.get(@url)
+    xml_doc  = Nokogiri::XML::Document.parse(results)
+    
+    title = title_from_feed(xml_doc)
+    content = ''
+    chapters = {}
+
+    xml_doc.search(feed_item_element(xml_doc)).each do |entry|
+      post = contents_from_entry(entry)
+      post_chapter = "chap#{(chapters.length + 1)}"
+      chapters[post[:title]] = post_chapter
+      seperator = "<br/><hr/><a name='#{post_chapter}' /><h3><a href='#{post[:link]}'>#{post[:title]}</a></h3>"
+      seperator += "<div>updated: #{post[:date]}</div><br/>"
+      content   += "#{seperator}#{post[:content]}<mbp:pagebreak />"
+    end
+
+    book_start       = "<a name='start' /><h3>#{title}</h3>"
+    table_of_content = "<a name='TOC' /><h3>Table of Contents</h3><ul>"
+    chapters.each_pair do |post_title, chapter|
+      table_of_content += "<li><a href='##{chapter}'>#{post_title}</a></li>"
+    end
+    table_of_content += "</ul><hr/><mbp:pagebreak />"
+    content = "#{book_start}#{table_of_content}#{content}"
+
+    {:title => title, :content => content}
+  end
+
+  private
+
+  def title_from_feed(xml_doc)
+    title = xml_doc.search('title').first.content
+  end
+
+  def link_from_entry(entry)
+    entry.search('link').first['href'] || entry.search('link').first.content
+  end
+
+  def date_from_entry(entry)
+    if entry.search('updated').length > 0
+      post_date = entry.search('updated').first.content
+      post_date = Date.parse(post_date).strftime("%m/%d/%Y at %I:%M%p")
+    else
+      post_date = entry.search('pubDate').first.content
+      post_date = Date.parse(post_date).strftime("%m/%d/%Y at %I:%M%p")
+    end
+  end
+
+  def content_from_entry(entry)
+    if entry.search('content').length > 0
+      entry.search('content').first.content
+    elsif entry.search('description').length > 0
+      entry.search('description').map(&:content).join("\n")
+    else
+      entry.at_xpath("content:encoded").text
+    end
+  end
+
+  def contents_from_entry(entry)
+    post_title   = entry.search('title').first.content
+    post_content = content_from_entry(entry)
+    post_date    = date_from_entry(entry)
+    post_link    = link_from_entry(entry)
+    {
+      :title => post_title,
+      :content => post_content,
+      :date => post_date,
+      :link => post_link
+    }
+  end
+
+  def feed_item_element(xml_doc)
+    if xml_doc.search("entry").length > 1
+      'entry'
+    else
+      'item'
+    end
+  end
 
 end
